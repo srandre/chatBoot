a589b05b0266beca1f7d508c829c0432d2bb3cc8
srandre <andrelima1607@gmail.com>
2022-02-28 22:49:49 -0300

    Added materials

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './interface/sidebar/sidebar.component';
 import { ChatBoxComponent } from './interface/chat-box/chat-box.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ChatBoxComponent } from './interface/chat-box/chat-box.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
